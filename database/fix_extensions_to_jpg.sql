0e804b9df77f749a8e01222be5b78da04194c6ac
abdoghh6677 <abdoghh6677@gmail.com>
2026-04-02 07:38:14 +0200

    Add SQL to fix extensions in existing Cloudinary URLs to .jpg

--- database/fix_extensions_to_jpg.sql
+++ database/fix_extensions_to_jpg.sql
@@ -19,3 +19,15 @@ UPDATE promotions
 SET image_url = 'https://res.cloudinary.com/dpjxle26o/image/upload/' || regexp_replace(image_url, '\.[^.]+$', '.jpg')
 WHERE image_url IS NOT NULL
 AND image_url NOT LIKE 'http%';
+
+-- Fix existing Cloudinary URLs to have .jpg extension
+UPDATE menu_items
+SET image_url = regexp_replace(image_url, '\.[^.]+$', '.jpg')
+WHERE image_url LIKE 'https://res.cloudinary.com/dpjxle26o/image/upload/%'
+AND image_url NOT LIKE '%.jpg';
+
+-- Fix existing Cloudinary URLs to have .jpg extension for promotions
+UPDATE promotions
+SET image_url = regexp_replace(image_url, '\.[^.]+$', '.jpg')
+WHERE image_url LIKE 'https://res.cloudinary.com/dpjxle26o/image/upload/%'
+AND image_url NOT LIKE '%.jpg';
