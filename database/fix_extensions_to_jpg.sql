8674d3ceb9df335f8be6cb39b9618c768247f604
abdoghh6677 <abdoghh6677@gmail.com>
2026-04-02 07:34:46 +0200

    Update SQL to also fix filename records to .jpg extension

--- database/fix_extensions_to_jpg.sql
+++ database/fix_extensions_to_jpg.sql
@@ -7,3 +7,15 @@ WHERE image_url LIKE 'https://ujxwbcsxeoprekzzkyuz.supabase.co/storage/v1/object
 UPDATE promotions
 SET image_url = 'https://res.cloudinary.com/dpjxle26o/image/upload/' || regexp_replace(split_part(image_url, '/', -1), '\.[^.]+$', '.jpg')
 WHERE image_url LIKE 'https://ujxwbcsxeoprekzzkyuz.supabase.co/storage/v1/object/public/%';
+
+-- Fix filename records to Cloudinary URLs with .jpg extension
+UPDATE menu_items
+SET image_url = 'https://res.cloudinary.com/dpjxle26o/image/upload/' || regexp_replace(image_url, '\.[^.]+$', '.jpg')
+WHERE image_url IS NOT NULL
+AND image_url NOT LIKE 'http%';
+
+-- Fix filename records to Cloudinary URLs with .jpg extension for promotions
+UPDATE promotions
+SET image_url = 'https://res.cloudinary.com/dpjxle26o/image/upload/' || regexp_replace(image_url, '\.[^.]+$', '.jpg')
+WHERE image_url IS NOT NULL
+AND image_url NOT LIKE 'http%';
